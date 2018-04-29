2aa1b17422f0c6ba0e0f8486c62442e198fc2486
yasunakacho <yasu1987@me.com>
2018-04-29 11:15:13 +0900

    second commit

--- astroplot/urls.py
+++ astroplot/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.urls import include, path
+from web import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^detail/(?P<id>.+)/$', views.detail, name='detail')
 ]
