0164e27b9446b88da08ae0dfa0477939a64c224b
yasunakacho <yasu1987@me.com>
2018-04-29 12:11:52 +0900

    sixth commit

--- astroplot/settings.py
+++ astroplot/settings.py
@@ -116,13 +116,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'web/static'),
+#]
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
