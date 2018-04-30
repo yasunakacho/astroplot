9feba4199cef39292cb8ae9cc6eaad7960184e4b
yasunakacho <yasu1987@me.com>
2018-04-30 18:33:10 +0900

    added cryptocurrency icons

--- web/views.py
+++ web/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
-import coinmarketcap
+from coinmarketcap import Market
+#import coinmarketcap
 
 # Create your views here.
 def home(request):
-    market = coinmarketcap.Market()
-    json_result = market.ticker('', limit=10, convert='USD')
+    coinmarketcap = Market()
+    json_result = coinmarketcap.ticker(start=0, limit=10, convert='USD')
 
     result = []
     for j in json_result:
-        result_line = []
+#        result_line = []
         result_line = {
             "id": j['id'],
             "name": j['name'],
+            "symbol": j['symbol'],
+            "rank": j['rank'],
             "price_usd": j['price_usd'],
+            "market_cap_usd": j['market_cap_usd'],
             "percent_change_24h": j['percent_change_24h'],
             "percent_change_7d": j['percent_change_7d'],
         }
@@ -20,8 +24,8 @@ def home(request):
     return render(request, 'home.html',{'result':result})
 
 def detail(request, id):
-    market = coinmarketcap.Market()
-    json_result = market.ticker(id, limit=10, convert='USD')
+    coinmarketcap = Market()
+    json_result = coinmarketcap.ticker(id, limit=10, convert='USD')
     return render(request, 'detail.html', {'json_result':json_result[0]})
 
 #print(result)
