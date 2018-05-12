ef7384bf86a6a6663e62deec28107a1de8cd67f8
yasunakacho <yasu1987@me.com>
2018-05-12 15:35:00 +0900

    changed static setting

--- astroplot/urls.py
+++ astroplot/urls.py
@@ -21,6 +21,5 @@ from web import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
-    url(r'^detail/(?P<id>.+)/$', views.detail, name='detail'),
-    url(r'^static/(?P<id>.+)/$', views.detail, name='static')
+    url(r'^detail/(?P<id>.+)/$', views.detail, name='detail')
 ]
