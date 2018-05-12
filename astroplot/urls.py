23c0eefa5ff34335232509ab49b2ed0f94505456
yasunakacho <yasu1987@me.com>
2018-05-12 16:20:22 +0900

    changed static setting

--- astroplot/urls.py
+++ astroplot/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 from web import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^detail/(?P<id>.+)/$', views.detail, name='detail')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
