edeabf05a86f2b49b0ef8865d9625d6e7a968839
yasunakacho <yasu1987@me.com>
2018-12-23 17:42:55 +0900

    added script to save scraped data to database expect for bitcoin

--- postgres/postgres_load.sql
+++ postgres/postgres_load.sql
@@ -1,2 +1,14 @@
- \copy web_price FROM 'bitcoin-sv.csv' WITH CSV;
  \copy web_price FROM 'bitcoin.csv' WITH CSV;
+ \copy web_price FROM 'ethereum.csv' WITH CSV;
+ \copy web_price FROM 'ripple.csv' WITH CSV;
+ \copy web_price FROM 'bitcoin-cash.csv' WITH CSV;
+ \copy web_price FROM 'eos.csv' WITH CSV;
+ \copy web_price FROM 'litecoin.csv' WITH CSV;
+ \copy web_price FROM 'stellar.csv' WITH CSV;
+ \copy web_price FROM 'iota.csv' WITH CSV;
+ \copy web_price FROM 'cardano.csv' WITH CSV;
+ \copy web_price FROM 'tron.csv' WITH CSV;
+ \copy web_price FROM 'neo.csv' WITH CSV;
+ \copy web_price FROM 'monero.csv' WITH CSV;
+ \copy web_price FROM 'tether.csv' WITH CSV;
+ \copy web_price FROM 'bitcoin-sv.csv' WITH CSV;
